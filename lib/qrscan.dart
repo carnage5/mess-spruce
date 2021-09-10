34db16d2801133882084f45974f6eddeb8a73367
kiran reddy <kiranreddykalakota2003@gmail.com>
2021-09-10 18:50:15 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/qrscan.dart
+++ lib/qrscan.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/platform_wrapper.dart';
 import 'dart:async';
@@ -15,20 +18,21 @@ class qrscan extends StatefulWidget {
 }
 
 class _qrscanState extends State<qrscan> {
-  String result ="Waiting...";
+  String result = "Waiting...";
   String? messcheck;
+
   Future _scanqr() async {
     try {
-      ScanResult  qrResult = await BarcodeScanner.scan() ;
+      ScanResult qrResult = await BarcodeScanner.scan();
       setState(() {
-        result = qrResult.rawContent  ;
-        if(result==messcheck) // compare the mess in the database to the scanned mess here
-        {
+        result = qrResult.rawContent;
+        if (result == messcheck) // compare the mess in the database to the scanned mess here
+            {
           Navigator.pushNamed(context, '/payment');
           result = "Waiting....";
         }
         else
-          result="You do not belong to this mess";
+          showAlertDialog(context, result);
       });
     } on PlatformException catch (ex) {
       if (ex.code == BarcodeScanner.cameraAccessDenied) {
@@ -41,16 +45,18 @@ class _qrscanState extends State<qrscan> {
           result = 'unknown error $ex';
         });
       }
-    }on FormatException  {
+    } on FormatException {
       setState(() {
-        result ="you pressed back ";
+        result = "you pressed back ";
       });
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    String? srn=widget.pass; //users srn is stored here , use it to search the db and get mess alloted
-    messcheck=getMess(srn); //store the alloted mess in this variable
+    String? srn = widget
+        .pass; //users srn is stored here , use it to search the db and get mess alloted
+    messcheck = getMess(srn); //store the alloted mess in this variable
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -62,24 +68,67 @@ class _qrscanState extends State<qrscan> {
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          result,
-          style: TextStyle(
-            fontSize: 25.0,
-            color: Colors.pink,
-          ),
-        ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('You belong to :',
+                  style: TextStyle(
+                    //fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  )),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(100.0),
+              child: Text('$messcheck',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 38.0,
+                    color: Colors.deepOrange,
+                  )),
+            ),
+            Text('$result'),
+          ]
       ),
+
       floatingActionButton: FloatingActionButton.extended(
-        onPressed:(){
+        onPressed: () {
           _scanqr();
         },
-        icon:Icon(Icons.camera_alt_rounded),
+        icon: Icon(Icons.camera_alt_rounded),
         label: Text('Scan Now'),
         backgroundColor: Colors.amberAccent,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  showAlertDialog(BuildContext context, String? mess) {
+    // Create button
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    // Create AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Alert"),
+      content: Text("$mess was scanned please scan alloted mess"),
+      actions: [
+        okButton,
+      ],
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
